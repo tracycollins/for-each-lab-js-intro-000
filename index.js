f132982823b226d76f02de9a8da089a5cbb03730
Tracy Collins <tc@threeceemedia.com>
2020-02-27 08:09:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach(function(element,index, array){
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = ["this", "that", "the other"]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+  return array
+}
